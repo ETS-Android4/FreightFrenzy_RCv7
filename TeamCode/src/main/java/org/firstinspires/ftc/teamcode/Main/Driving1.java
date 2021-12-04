d2da8642c08dde37bfb1cad84ac90e4d4afdaeca
Lucian <dracea.lucian.mihai@gmail.com>
2021-12-04 11:18:06 +0200

    04.12.2021

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/Driving1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main/Driving1.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.Main;
 
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.hardware.lynx.commands.core.LynxReadVersionStringResponse;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -12,11 +14,20 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.internal.camera.delegating.DelegatingCaptureSequence;
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.RoadRunner.util.BNO055IMUUtil;
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.DriveConstants;
+
+
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 
 import java.nio.file.attribute.FileOwnerAttributeView;
 import java.util.function.Function;
 
-@TeleOp(name = "Brat1", group = "main")
+@TeleOp(name = "Main_Driving", group = "main")
 
 public class Driving1 extends LinearOpMode {
 
@@ -25,10 +36,12 @@ public class Driving1 extends LinearOpMode {
     private DcMotorEx motor_slider;
     private DcMotorEx motor_carusel;
     private DcMotorEx motor_colector;
-    private DcMotorEx frontLeft, frontRight, backLeft, backRight;
 
+    private int orientation_drive = -1;
+    private SampleMecanumDrive mecanum_drive;
+    private FtcDashboard dashboard = FtcDashboard.getInstance();
 
-    private Servo holder;
+    private CRServo holder;
 
     private functions fx = new functions();
 
@@ -68,36 +81,16 @@ public class Driving1 extends LinearOpMode {
         motor_colector.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor_colector.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        //Init pentru motoarele de la roti
-        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
-        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
-        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
-
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
 
         //Init pentru servo-ul de la holder de elemente
-        holder = hardwareMap.servo.get("holder");
-        holder.setDirection(Servo.Direction.FORWARD);
-        holder.setPosition(0.1);
+        holder = hardwareMap.crservo.get("holder");
+        holder.setDirection(CRServo.Direction.REVERSE);
+
+
+        //Init pentru mecanum drive RR
+        mecanum_drive = new SampleMecanumDrive(hardwareMap);
+
+        mecanum_drive.setPoseEstimate(new Pose2d(0, 0, 0));
 
         while (!opModeIsActive() && !isStopRequested())
         {
@@ -109,16 +102,33 @@ public class Driving1 extends LinearOpMode {
 
         while (opModeIsActive())
         {
+            if (gamepad2.x && orientation_drive == -1)
+            {
+                orientation_drive = 1;
+                sleep(fx.button_sleep);
+            }
+            else if (gamepad2.x && orientation_drive == 1)
+            {
+                orientation_drive = -1;
+                sleep(fx.button_sleep);
+            }
+
+            mecanum_drive.setWeightedDrivePower(
+                    new Pose2d(
+                            orientation_drive*gamepad2.left_stick_y,
+                            orientation_drive*gamepad2.left_stick_x,
+                            -gamepad2.right_stick_x
+                    )
+            );
+            mecanum_drive.update();
+
             fx.brat(true);
             fx.slider(true);
             fx.carusel();
             fx.colector();
             fx.holder_cr(false);
-            fx.smoothMovement(true);
-            if (gamepad1.b) {
-                fx.reset_brat();
-                sleep(fx.button_sleep);
-            }
+            fx.reset_brat();
+
 
             telemetry.update();
         }
@@ -126,173 +136,133 @@ public class Driving1 extends LinearOpMode {
     }
 
     class functions {
-        private int button_sleep = 75;
+        private int button_sleep = 135;
+
+        private boolean resetting_brat = false;
 
         private int upper_limit_brat = -130;
-        private int lower_limit_brat = 3060;
+        private int lower_limit_brat = 2300;
         private boolean clear_brat = false;
+        private int dist_min_ext = 900;
+        private double put_brat = 0.8;
 
         public void brat(boolean logs) {
-            if (poz_slider * -1 >= 1000)
+            if (poz_slider >= dist_min_ext)
                 clear_brat = true;
             else
                 clear_brat = false;
 
-            if (gamepad1.dpad_down && motor_brat.getCurrentPosition() <= lower_limit_brat && clear_brat)
-                motor_brat.setPower(0.75);
-            else if (gamepad1.dpad_up && motor_brat.getCurrentPosition() >= upper_limit_brat && clear_brat)
-                motor_brat.setPower(-0.75);
+            if (poz_slider >= 1800)
+                put_brat = 0.5;
             else
-                motor_brat.setPower(0);
+                put_brat = 0.8;
+
+            if (resetting_brat == false) {
+                if (gamepad1.dpad_down && motor_brat.getCurrentPosition() <= lower_limit_brat && clear_brat)
+                    motor_brat.setPower(put_brat);
+                else if (gamepad1.dpad_up && motor_brat.getCurrentPosition() >= upper_limit_brat && clear_brat)
+                    motor_brat.setPower(-put_brat);
+                else
+                    motor_brat.setPower(0);
+            }
 
             if (logs) {
                 telemetry.addData("Pozitie Curenta Motor Brat: ", motor_brat.getCurrentPosition());
-                telemetry.addData("Putere Teoretica Motor Brat: ", 0.75);
+                telemetry.addData("Putere Teoretica Motor Brat: ", put_brat);
                 telemetry.addData("Putere Practica Motor Brat: ", motor_brat.getPower());
             }
         }
 
-        public void reset_brat() {
-            int semn = 1;
 
-            while (motor_brat.getCurrentPosition() >= 20 || motor_brat.getCurrentPosition() <= -20) {
-                if (motor_brat.getCurrentPosition() >= 0)
-                    semn = -1;
-                motor_brat.setPower(0.5 * semn);
+        public void reset_brat() {
+            if (gamepad1.b) {
+                resetting_brat = true;
+                motor_brat.setTargetPosition(0);
+                motor_slider.setTargetPosition(0);
+
+                motor_brat.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                motor_slider.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                if (motor_brat.isBusy())
+                    motor_brat.setPower(1);
+
+                if (motor_slider.isBusy())
+                    motor_slider.setPower(1);
+
+                sleep(button_sleep);
             }
+
+            if (!motor_slider.isBusy() && !motor_brat.isBusy())
+                resetting_brat = false;
+
+            if (resetting_brat == false) {
+                motor_brat.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                motor_slider.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+
         }
 
         private int poz_slider = 0;
-        private int upper_limit_slider = 5450;
-        private int lower_limit_slider = 5;
+        private int upper_limit_slider = 5350;
+        private int lower_limit_slider = 25;
+        private double put_slider = 1;
 
         public void slider(boolean logs) {
-            poz_slider = motor_slider.getCurrentPosition();
-            if (gamepad1.right_trigger >= 0.3 && poz_slider * -1 <= upper_limit_slider) {
-                motor_slider.setPower(-1);
-            } else if (gamepad1.left_trigger >= 0.3 && poz_slider * -1 >= lower_limit_slider) {
-                motor_slider.setPower(1);
-            } else {
-                motor_slider.setPower(0);
+            poz_slider = motor_slider.getCurrentPosition()*-1;
+
+            if (resetting_brat == false) {
+                if (gamepad1.right_trigger >= 0.3 && poz_slider <= upper_limit_slider) {
+                    motor_slider.setPower(-put_slider);
+                } else if (gamepad1.left_trigger >= 0.3 && poz_slider >= lower_limit_slider) {
+                    motor_slider.setPower(put_slider);
+                } else
+                    motor_slider.setPower(0);
             }
 
             if (logs) {
-                telemetry.addData("Pozitie Curenta Motor Slider:", motor_slider.getCurrentPosition() * -1);
-                telemetry.addData("Putere Teoretica Motor Slider:", 0.4);
+                telemetry.addData("Pozitie Curenta Motor Slider:", poz_slider);
+                telemetry.addData("Putere Teoretica Motor Slider:", put_slider);
                 telemetry.addData("Putere Practica Motor Slider:", motor_slider.getPower());
             }
 
         }
 
+        private boolean carusel_tgl = false;
         public void carusel() {
 
-            if (gamepad1.x && motor_carusel.getPower() <= 0.5) {
+            if (gamepad1.x && !carusel_tgl) {
                 motor_carusel.setPower(1);
+                carusel_tgl = true;
                 sleep(button_sleep);
-            } else if (gamepad1.x && motor_carusel.getPower() >= 0.5) {
+            } else if (gamepad1.x && carusel_tgl) {
                 motor_carusel.setPower(0);
+                carusel_tgl = false;
                 sleep(button_sleep);
             }
 
         }
 
         public void colector() {
-            if (gamepad1.y && motor_colector.getPower() == 0) {
+
+            if (gamepad2.y && motor_colector.getPower() == 0 && poz_slider <= 100) {
                 motor_colector.setPower(1);
                 sleep(button_sleep);
-            } else if (gamepad1.y && motor_colector.getPower() == 1) {
+            } else if (gamepad2.y && motor_colector.getPower() == 1) {
                 motor_colector.setPower(0);
                 sleep(button_sleep);
             }
         }
 
+
+        private int clear_holder = 2950;
         public void holder_cr(boolean logs) {
-            if (gamepad1.left_bumper)
-                holder.setPosition(1);
-
+            if (gamepad1.left_bumper && poz_slider >= clear_holder)
+                holder.setPower(1);
+            else if (poz_slider >= clear_holder)
+                holder.setPower(-0.6);
             else
-                holder.setPosition(0.1);
+                holder.setPower(-1);
 
-        }
-
-        int viteza = 0;
-        double power = 0.8;
-        double halfPower = 0.5;
-
-        public void smoothMovement(boolean getLogs) {
-
-            if (getLogs) {
-                telemetry.addData("~~~~~~~~~~~~~~~~~~~~", "");
-                telemetry.addData("Front left ", frontLeft.getCurrentPosition());
-                telemetry.addData("Front right ", frontRight.getCurrentPosition());
-                telemetry.addData("Back left ", backLeft.getCurrentPosition());
-                telemetry.addData("Back right ", backRight.getCurrentPosition());
-                telemetry.addData("~~~~~~~~~~~~~~~~~~~~", "");
-            }
-
-            if (gamepad2.right_bumper && viteza == 0) {
-                viteza = 1;
-                sleep(200);
-            } else if (gamepad2.right_bumper && viteza == 1) {
-                viteza = 0;
-                sleep(200);
-            }
-
-            if (viteza == 0) {
-                if (gamepad2.dpad_up) {
-                    frontLeft.setPower(power + gamepad2.right_stick_x / 2);
-                    frontRight.setPower(power - gamepad2.right_stick_x / 2);
-                    backRight.setPower(power - gamepad2.right_stick_x / 2);
-                    backLeft.setPower(power + gamepad2.right_stick_x / 2);
-                } else if (gamepad2.dpad_down) {
-                    frontLeft.setPower(-power + gamepad2.right_stick_x / 2);
-                    frontRight.setPower(-power - gamepad2.right_stick_x / 2);
-                    backRight.setPower(-power - gamepad2.right_stick_x / 2);
-                    backLeft.setPower(-power + gamepad2.right_stick_x / 2);
-                } else if (gamepad2.dpad_right) {
-                    frontLeft.setPower(power + gamepad2.right_stick_x / 2);
-                    frontRight.setPower(-power - gamepad2.right_stick_x / 2);
-                    backRight.setPower(power - gamepad2.right_stick_x / 2);
-                    backLeft.setPower(-power + gamepad2.right_stick_x / 2);
-                } else if (gamepad2.dpad_left) {
-                    frontLeft.setPower(-power + gamepad2.right_stick_x / 2);
-                    frontRight.setPower(power - gamepad2.right_stick_x / 2);
-                    backRight.setPower(-power - gamepad2.right_stick_x / 2);
-                    backLeft.setPower(power + gamepad2.right_stick_x / 2);
-                } else {
-                    frontLeft.setPower(+gamepad2.right_stick_x + frontLeft.getPower() / 4);
-                    frontRight.setPower(-gamepad2.right_stick_x + frontRight.getPower() / 4);
-                    backRight.setPower(-gamepad2.right_stick_x + backRight.getPower() / 4);
-                    backLeft.setPower(+gamepad2.right_stick_x + backLeft.getPower() / 4);
-                }
-            } else if (viteza == 1) {
-                if (gamepad2.dpad_up) {
-                    frontLeft.setPower(halfPower + gamepad2.right_stick_x / 2);
-                    frontRight.setPower(halfPower - gamepad2.right_stick_x / 2);
-                    backRight.setPower(halfPower - gamepad2.right_stick_x / 2);
-                    backLeft.setPower(halfPower + gamepad2.right_stick_x / 2);
-                } else if (gamepad2.dpad_down) {
-                    frontLeft.setPower(-halfPower + gamepad2.right_stick_x / 2);
-                    frontRight.setPower(-halfPower - gamepad2.right_stick_x / 2);
-                    backRight.setPower(-halfPower - gamepad2.right_stick_x / 2);
-                    backLeft.setPower(-halfPower + gamepad2.right_stick_x / 2);
-                } else if (gamepad2.dpad_right) {
-                    frontLeft.setPower(halfPower + gamepad2.right_stick_x / 2);
-                    frontRight.setPower(-halfPower - gamepad2.right_stick_x / 2);
-                    backRight.setPower(halfPower - gamepad2.right_stick_x / 2);
-                    backLeft.setPower(-halfPower + gamepad2.right_stick_x / 2);
-                } else if (gamepad2.dpad_left) {
-                    frontLeft.setPower(-halfPower + gamepad2.right_stick_x / 2);
-                    frontRight.setPower(halfPower - gamepad2.right_stick_x / 2);
-                    backRight.setPower(-halfPower - gamepad2.right_stick_x / 2);
-                    backLeft.setPower(halfPower + gamepad2.right_stick_x / 2);
-                } else {
-                    frontLeft.setPower(+gamepad2.right_stick_x / 2 + frontLeft.getPower() / 2);
-                    frontRight.setPower(-gamepad2.right_stick_x / 2 + frontRight.getPower() / 2);
-                    backRight.setPower(-gamepad2.right_stick_x / 2 + backRight.getPower() / 2);
-                    backLeft.setPower(+gamepad2.right_stick_x / 2 + backLeft.getPower() / 2);
-                }
-            }
         }
     }
 }
